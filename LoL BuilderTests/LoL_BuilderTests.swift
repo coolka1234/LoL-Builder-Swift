e3daed7c3d70409dc1e3a91ca606f3c308479b64
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-11-17 22:17:39 +0100

    baseline commit

---
+++ LoL BuilderTests/LoL_BuilderTests.swift
@@ -0,0 +1,16 @@
+//
+//  LoL_BuilderTests.swift
+//  LoL BuilderTests
+//
+//  Created by Coolka on 17/11/2024.
+//
+
+import Testing
+
+struct LoL_BuilderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
