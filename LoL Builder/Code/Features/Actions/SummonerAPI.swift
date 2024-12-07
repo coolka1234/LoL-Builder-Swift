45a5b0a0b5d2aa85d216324966766847aacb5573
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-12-08 00:04:26 +0100

    work on api for champs now

--- LoL Builder/Code/Features/Actions/SummonerAPI.swift
+++ LoL Builder/Code/Features/Actions/SummonerAPI.swift
@@ -14,6 +14,7 @@ struct Summoner: Decodable {
     let profileIconId: Int
     let summonerLevel: Int
 }
+//good luck with builds lol
 
 class SummonerService {
     func getSummonerInfo(byName name: String, completion: @escaping (Result<Summoner, NetworkError>) -> Void) {
